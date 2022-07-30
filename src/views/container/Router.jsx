e07d93c8e9e29238c8872169bfbda063d28b70e1
Florin Cioroianu <florincioroianu23@gmail.com>
2022-07-30 20:44:02 +0300

    first commit

--- src/views/container/Router.jsx
+++ src/views/container/Router.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-  } from "react-router-dom";
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import VerifyEmail from '../pages/VerifyEmail';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ChangePassword from "../pages/ChangePassword";
+import ForgotPassword from "../pages/ForgotPassword";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import VerifyEmail from "../pages/VerifyEmail";
 
 const Router = () => {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/login" element={<Login />}/>
-            <Route path="/register" element={<Register />}/>
-            <Route path="verify-email" element={<VerifyEmail/>} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="verify-email" element={<VerifyEmail />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="change-password" element={<ChangePassword />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
